e68e09edefb62a013a44c8068cc7563c8f638c0d
Effluvia <74818407+Effluvia@users.noreply.github.com>
2023-06-28 00:43:55 +0300

    Refactor

--- Official/PARANOIA/ep2-Sources/1_onlyent(PARANOIA).bat
+++
@@ -1,9 +0,0 @@
-@echo off 
-set MAPSPATH=D:\Mapping\maps
-set MAPNAME=1 
-set CSPATH=D:\Games\cs1.6\paranoia\maps
- 
-D:\Mapping\Zhlt\hlcsg.exe -texdata 8192 -onlyents D:\Mapping\maps\1.map 
-
-
-echo COMPILE END
--- Official/from 17BUDDIES (05.09.21)/Spongebob_Squarepants_Models_Prefabs_Sky_Wad/Spongebob Squarepants Models and Prefabs/Sky/Readme.txt
+++
@@ -1,7 +0,0 @@
-Bikini Bottom Skybox By AkShit (GenoCide)
-
-Paste files in cstrike\gfx\env folder
-
-If you use this mod in your map, a little credit will be appreciated.
-
-Happy Mapping!!
