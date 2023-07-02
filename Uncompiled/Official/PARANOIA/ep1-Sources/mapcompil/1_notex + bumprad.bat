944c2fa76ff9631ebdb23f7760ce07aa8148c516
Effluvia <74818407+Effluvia@users.noreply.github.com>
2023-07-02 15:19:54 +0300

    Mega-Patch

--- Uncompiled/Official/PARANOIA/ep1-Sources/mapcompil/1_notex + bumprad.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-set MAPSPATH=d:\[Mapping]\maps
-set MAPNAME=1 
-set CSPATH=d:\Games\Half-life\valve\maps
-
-d:\[Mapping]\Zhlt\bumprad.exe -texdata 8192 -extra -estimate d:\[Mapping]\maps\1.map
-
-echo COMPILE END
